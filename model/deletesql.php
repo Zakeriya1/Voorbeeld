1855696acc185a987c60581b1dc2871b7ec5b951
Zakeriya1 <227801@edu.rocfriesepoort.nl>
2023-03-06 10:32:39 +0100

    Debug voorbeeld

---
+++ model/deletesql.php
@@ -0,0 +1,44 @@
+<?php
+
+// Include config file
+require_once "../include/config.php";
+
+// Process delete operation after confirmation
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+
+    
+    // Prepare a delete statement
+    $sql = "DELETE FROM employees WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+          // Set parameters
+          $param_id = trim($_POST["id"]);
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+      
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            // Records deleted successfully. Redirect to landing page
+            header("location: ../index.php");
+            exit();
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // Check existence of id parameter
+    if(empty(trim($_GET["id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: ../include/error.php");
+        exit();
+    }
+}
+?>
